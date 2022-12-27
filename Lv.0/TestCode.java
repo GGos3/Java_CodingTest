74f567f6dfe85bd16c2cae7c0be9673788e11115
JangFeel <janpll0421@gmail.com>
2022-12-28 02:08:00 +0900

    TestCode

---
+++ Lv.0/TestCode.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class TestCode {
+    public static void main(String[] args) {
+        약수의_합 test = new 약수의_합();
+
+        System.out.println(test.solution(12));
+    }
+}
