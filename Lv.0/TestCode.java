bc6d0a52227edd147910eef8a169143aed204a6b
GGos3 <janpll0421@gmail.com>
2023-01-01 22:49:14 +0900

    2일차

--- Lv.0/TestCode.java
+++ Lv.0/TestCode.java
@@ -2,8 +2,9 @@ import java.util.Arrays;
 
 public class TestCode {
     public static void main(String[] args) {
-        약수의_합 test = new 약수의_합();
+        평균_구하기 test = new 평균_구하기();
 
-        System.out.println(test.solution(12));
+        System.out.println(test.solution(new int[]{5, 5}));
+
     }
 }
