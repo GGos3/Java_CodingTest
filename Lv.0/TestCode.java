59539235531fa781d58c62e81273f1682f2e7723
GGos3 <janpll0421@gmail.com>
2023-01-02 02:21:15 +0900

    init

--- Lv.0/TestCode.java
+++ Lv.0/TestCode.java
@@ -2,9 +2,6 @@ import java.util.Arrays;
 
 public class TestCode {
     public static void main(String[] args) {
-        자릿수_더하기 test = new 자릿수_더하기();
 
-        int solution = test.solution(987);
-        System.out.println("solution = " + solution);
     }
 }
