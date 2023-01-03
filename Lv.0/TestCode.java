db30159cd14c7a49ac6e230d359af76a6892f91b
GGos3 <janpll0421@gmail.com>
2023-01-04 02:04:34 +0900

    4일차

--- Lv.0/TestCode.java
+++ Lv.0/TestCode.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 
 public class TestCode {
     public static void main(String[] args) {
-        System.out.println("args = " + Arrays.toString(args));
+        서울에서_김서방_찾기 test = new 서울에서_김서방_찾기();
+
+        System.out.println(test.solution(new String[]{"wwEW", "wee", "Kim"}));
     }
 }
