69cd6095aea1c29cab52bab01c19ad58ea3cad03
GGos3 <janpll0421@gmail.com>
2023-01-04 02:05:11 +0900

    서울에서 김서방 찾기

---
+++ Lv.1/서울에서_김서방_찾기.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class 서울에서_김서방_찾기 {
+    public String solution(String[] seoul) {
+        return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") + "에 있다";
+    }
+}
